c397716bcf5928f152185d16fc99750fad45093c
cangling43 <2861268484@qq.com>
2024-07-10 10:14:00 +0800

    增加查询房源功能

--- src/views/user/page2.vue
+++ src/views/user/page2.vue
@@ -19,6 +19,12 @@
                     <router-view></router-view>
                 </div>
             </el-main>
+            <el-footer class="footer">
+                <el-pagination class="pagin" background layout="total, sizes, prev, pager, next, jumper"
+                    :current-page="tablePage.pageNum" :page-size="tablePage.pageSize" :page-sizes="pageSizes"
+                    :total="tablePage.total" @size-change="handleSizeChange" @current-change="handlePageChange" />
+            </el-footer>
+            <br>
         </el-container>
     </div>
 </template>
@@ -29,7 +35,26 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-
+    data() {
+        return {
+            tablePage: {
+                pageNum: 1, // 第几页
+                pageSize: 10, // 每页多少条
+                total: 100 // 总记录数
+            },
+            pageSizes: [10, 20, 30]
+        }
+    },
+    methods: {
+        handlePageChange(currentPage) {
+            this.tablePage.pageNum = currentPage
+            // 在此刷新数据
+        },
+        handleSizeChange(pageSize) {
+            this.tablePage.pageSize = pageSize
+            // 在此刷新数据
+        }
+    }
 })
 
 </script>
@@ -53,9 +78,19 @@ export default defineComponent({
         display: flex;
         flex-direction: row;
         /* 设置水平排布 */
-        flex-wrap: wrap; /* 允许自动换行 */
+        flex-wrap: wrap;
+        /* 允许自动换行 */
         gap: 60px;
         /* 设置子元素之间的间距 */
+    }
+}
+
+.footer {
+    display: flex;
+    justify-content: center;
+
+    .pagin {
+        align-items: center;
     }
 }
 </style>
--- src/views/user/search_module.vue
+++ src/views/user/search_module.vue
@@ -1,28 +1,33 @@
 <template>
-    <el-card class="content-card" shadow="hover"  @click="$router.push('detailup')">
+    <el-card class="content-card" shadow="hover" @click="$router.push('detailup')">
+        <div class="flex gap-2 mt-4">
+            <el-tag :key="this.homeInfo.item.label" :type="this.homeInfo.item.type" effect="dark" round>
+                {{ this.homeInfo.item.label }}
+            </el-tag>
+        </div>
         <img src="../../../public/image/房屋内部图.jpg" alt="房屋内部图">
         <div class="content-card-inner">
-            <h3>{{ type }}</h3>
+            <h3>{{ this.homeInfo.format }}</h3>
             <div class="icon">
                 <el-icon>
                     <Location />
-                </el-icon> {{ address }}
+                </el-icon> {{ this.homeInfo.address }}
             </div>
             <div class="money">
-                ￥ {{ money }} / 月
+                ￥ {{ this.homeInfo.money }} / 月
             </div>
         </div>
         <hr>
         <div class="content-card-down">
             <el-icon>
                 <House />
-            </el-icon>&nbsp;卧室:{{ bedchamber }}&nbsp;&nbsp;
+            </el-icon>&nbsp;卧室:{{ this.homeInfo.bedchamber }}&nbsp;&nbsp;
             <el-icon>
                 <Lock />
-            </el-icon>&nbsp;洗手间:{{ toilet }}&nbsp;&nbsp;
+            </el-icon>&nbsp;洗手间:{{ this.homeInfo.toilet }}&nbsp;&nbsp;
             <el-icon>
                 <School />
-            </el-icon>&nbsp;面积:{{ area }}
+            </el-icon>&nbsp;面积:{{ this.homeInfo.area }}
         </div>
     </el-card>
 </template>
@@ -31,14 +36,36 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
+    created() {
+        // this.getHomeInfo()
+    },
     data() {
         return {
-            address: '武汉 > 洪山区 > 光谷',
-            type: '王家湾 二环 两房',
-            money: '4000',
-            bedchamber: '3',
-            toilet: '2',
-            area: '148.0 m²'
+            homeInfo: {
+                address: '武汉 > 洪山区 > 光谷',
+                format: '王家湾 二环 两房',
+                money: '4000',
+                bedchamber: '3',
+                toilet: '2',
+                area: '148.0 m²',
+                item: {
+                    type: 'success', label: '正在出租'
+                }
+            }
+        }
+    },
+    methods: {
+        // async getHomeInfo() {
+        //     const { data: res } = await this.$http.get('home/info', { params: this.params })
+        //     if (res.code !== '200') {
+        //         this.$message.error('请求会员数据错误!')
+        //     } else {
+        //         this.info = res.data.list
+        //     }
+        // },
+        getHomeInfo() {
+            this.homeInfo.item.type = 'danger',
+            this.homeInfo.item.label = '已出租'
         }
     }
 })
@@ -46,11 +73,11 @@ export default defineComponent({
 </script>
 
 <style lang="less" scoped>
-
 .content-card {
     cursor: pointer;
     height: 400px;
     width: 300px;
+    position: relative;
 
     .content-card-inner {
         width: 100%;
@@ -70,6 +97,12 @@ export default defineComponent({
             font-size: 25px;
             color: #409EFF;
         }
+    }
+
+    .mt-4 {
+        position: absolute;
+        right: 10px;
+        top: 10px;
     }
 
 
